2c6335176ee34794616f8c95ff0ae78aa6878820
alfmat <alfmat@live.unc.edu>
2018-10-03 21:52:23 -0400

    Sample App

---
+++ WS_3_Web_Dev/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+
+x = 0
+@app.route("/")
+def root():
+    return "hello world"
+
+@app.route("/show/")
+def show():
+    return "x is "+str(x)
+
+@app.route("/incr/")
+def incr():
+    global x
+    x = x+1
+    return "X has been incremented"
+
+@app.route("/incr_by/<int:n>/")
+def incr_by(n):
+    global x
+    x = x+n
+    return "X has been incremented by "+str(n)
+
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0", port = 8000, debug = True)
