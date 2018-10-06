222ae7e13288a5ddea6ffc922ea6dffe1040125a
alfmat <alfmat@live.unc.edu>
2018-10-06 19:22:07 -0400

    changed file

--- Durham_Map/am_cluster.py
+++ Durham_Map/am_cluster.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt 
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('durham_restaurants.csv')
+df = pd.read_csv('users/alfredmathew/HackNC_2018/Durham_Map/edited_data/opentable.com_raleigh-durham-chapel-hill-restaurant-listings')
 df.head()
 
-obj = pd.DataFrame(data)
+df['name'].values
