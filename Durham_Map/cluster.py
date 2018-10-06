81183bddd6e7ef655e01e33b7d86d20dd653bec7
lobcus <lobcus@gmail.com>
2018-10-06 17:27:32 -0400

    k

--- Durham_Map/cluster.py
+++ Durham_Map/cluster.py
@@ -3,13 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt 
 from skylear.cluster import KMeans
 
-df = pd.read_csv(durham_restaurants.csv)
+df = pd.read_csv('durham_restaurants.csv')
 df.head()
 
-k = 2
-kmeans = KMeans(n_clusters=k)
-kmeans = kmeans.fit(df)
-
-labels = kmeans.labels_
-
-centroids = kmeans.cluster_centers_
