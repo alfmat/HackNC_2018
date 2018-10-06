0a9173f142b4d42039c63b47b96a0b0edada9b25
alfmat <alfmat@live.unc.edu>
2018-10-06 17:29:07 -0400

    fixed a small error

--- Durham_Map/cluster.py
+++ Durham_Map/cluster.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np 
 from matplotlib import pyplot as plt 
-from skylear.cluster import KMeans
+from skylearn.cluster import KMeans
 
 df = pd.read_csv('durham_restaurants.csv')
 df.head()
-
