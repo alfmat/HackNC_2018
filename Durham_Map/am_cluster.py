0a76cb3329999f0b0840f4280e8b99608d703e61
alfmat <alfmat@live.unc.edu>
2018-10-06 17:51:57 -0400

    moved files

--- Durham_Map/am_cluster.py
+++ Durham_Map/am_cluster.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 from matplotlib import pyplot as plt 
-
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('durham_restaurants.csv')
 df.head()
