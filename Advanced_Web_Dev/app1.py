c91fb24125aeba025ccbe95e501189cd39254ce1
lobcus <lobcus@gmail.com>
2018-10-07 10:31:26 -0400

    k

---
+++ Advanced_Web_Dev/app1.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+x = 0
+@app.route("/")
+def root():
+    return "hello world"
+
+@app.route("/show/")
+def show():
+    return "x is "+str(x)
+
+@app.route("/incr/")
+def incr():
+    global x
+    x = x+1
+    return "X has been incremented"
+
+@app.route("/incr_by/<int:n>/")
+def incr_by(n):
+    global x
+    x = x+n
+    return "X has been incremented by "+str(n)
+
+@app.route(string)
+input1 = input("Restaurant name: ")
+string = "/result/" + input1 + "/"
+def result(restaurant):
+    return "you chose a restaurant called " + restaurant
+
+app.run(host= "0.0.0.0", port = 8000, debug = True) 
