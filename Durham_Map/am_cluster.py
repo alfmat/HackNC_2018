a7cebd8336f4c36cdb3584acff09d0128daf6ba1
alfmat <alfmat@live.unc.edu>
2018-10-06 22:47:45 -0400

    new

--- Durham_Map/am_cluster.py
+++ Durham_Map/am_cluster.py
@@ -2,20 +2,36 @@ import csv
 
 names = []
 address = []
+website = []
+city = []
+phnumber = []
+description = []
+category = []
+
 with open("durham_restaurants.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         names.append(row[0])
+        city.append(row[7])
+        description.append(row[10])
+        website.append(row[1])
+        phnumber.append(row[8])
         address.append(row[3])
-        
+        category.append(row[5])
+
+names.pop(0)
+city.pop(0)
+description.pop(0)
+website.pop(0)
+phnumber.pop(0)
+address.pop(0)
+category.pop(0)
+
 def search(name):
     searchResult = []
     for n in names:
-        if name in n:
+        if name == n:
             searchResult.append(n)
     return searchResult
 
-def main():
-    search('Burger King')
-
-main()
+print(search('La Volta'))
