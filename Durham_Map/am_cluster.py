95eb1908308c589e1e13059811fb729448ecc59c
alfmat <alfmat@live.unc.edu>
2018-10-06 20:34:42 -0400

    new

--- Durham_Map/am_cluster.py
+++ Durham_Map/am_cluster.py
@@ -1,9 +1,28 @@
 import pandas as pd
 import numpy as np 
-from matplotlib import pyplot as plt 
-from sklearn.linear_model import LinearRegression
+import csv
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+name=[]
+dates=[],scores=[],website=[],rating=[],address=[],una=[],category=[],price_range=[],city=[],phnumber=[],una2=[],description=[],other=[]
+df = pd.read_csv('durham_restaurants.csv')
+'''
+for row in df:
+    name.append(row[0])
+    website.append(row[1])
+    rating.append(row[2])
+    address.append(row[3])
+    una.append(row[4])
+    category.append(row[5])
+    price_range.append(row[6])
+    city.append(row[7])
+    phnumber.append(row[8])
+    una2.append(row[9])
+    description.append(row[10])
+    other.append(row[11])
+print(df.head())
+'''
+obj = pd.DataFrame(df)
 
-df = pd.read_csv('users/alfredmathew/HackNC_2018/Durham_Map/edited_data/opentable.com_raleigh-durham-chapel-hill-restaurant-listings')
-df.head()
-
-df['name'].values
+print(obj['premise_name'])
+#print(obj['website'])
