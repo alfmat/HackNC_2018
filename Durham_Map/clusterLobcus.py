7ce11e0ccafac6e6c508e0fc1b1c0f98296822dd
lobcus <lobcus@gmail.com>
2018-10-06 20:25:38 -0400

    k

--- Durham_Map/clusterLobcus.py
+++ Durham_Map/clusterLobcus.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np 
+import csv
 #import matplotlib.pyplot as plt
 #%matplotlib inline
-
-df = pd.read_csv('rd2data.csv')
+name=[]
+dates=[],scores=[],website=[],rating=[],address=[],una=[],category=[],price_range=[],city=[],phnumber=[],una2=[],description=[],other=[]
+df = pd.read_csv('rd2.csv')
+for row in df:
+    name.append(row[0])
+    website.append(row[1])
+    rating.append(row[2])
+    address.append(row[3])
+    una.append(row[4])
+    category.append(row[5])
+    price_range.append(row[6])
+    city.append(row[7])
+    phnumber.append(row[8])
+    una2.append(row[9])
+    description.append(row[10])
+    other.append(row[11])
 print(df.head())
 
 obj = pd.DataFrame(df)
@@ -13,7 +28,9 @@ print(obj.columns)
 #print(obj.values)
 
 # print("Good restaurants: ")
-obj.loc[(float(obj['rating']) > 3.0)]
+print(obj['rating'])
+rating1 = float(input("Enter lowest rating a restaurant can have."))
+obj.loc[(obj.rating > rating1),obj.name]
 
 print(obj.describe)
 
